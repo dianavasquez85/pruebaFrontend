ee669d52d9cf233e5830ed6721f96741984332b4
dianavasquez85 <78568545+dianavasquez85@users.noreply.github.com>
2021-08-12 19:04:06 -0500

    Prueba frontend Diana Vásquez

---
+++ src/app/components/prestamo/prestamo.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-prestamo',
+  templateUrl: './prestamo.component.html',
+  styleUrls: ['./prestamo.component.css']
+})
+export class PrestamoComponent implements OnInit {
+
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+
+  }
+
+}
